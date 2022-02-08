51ec5ed3b1d337e360dc46dedfd056e419560583
Aramais Garibjan <aramo.g@hotmail.de>
2022-02-08 23:02:12 +0100

    create Quit-App

---
+++ style/components/card.stories.js
@@ -0,0 +1,11 @@
+import DefaultHtml from "./card.default.html";
+
+export default {
+  title: "components/card",
+};
+
+export const Default = () => DefaultHtml;
+
+export const Error = () => /* html */ `
+  <h1>Moin</h1>
+`;
