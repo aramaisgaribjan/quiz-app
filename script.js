eb249dec4378283b148d80b7122ae640bcdea390
Aramais Garibjan <aramo.g@hotmail.de>
2022-02-15 10:23:43 +0100

    add change button text on click

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const btnShowAnswer = document.querySelector(".btnShowAnswer");
+const answerText = document.querySelector(".answer")
 
 btnShowAnswer.addEventListener("click", () => {
-    window.alert('Die Antwort')
+    answerText.classList.toggle("answer")
   });
  
+  function change() 
+  {
+      var elem = document.getElementById("btnShowAnswer");
+      if (elem.value=="Hide Answer") elem.value = "Show Answer";
+      else elem.value = "Hide Answer";
+  }
