e4cc3d3856154162b2f93af280f6bc5e05ec1fa0
Aramais Garibjan <aramo.g@hotmail.de>
2022-02-18 14:26:44 +0100

    add querySelectorAll

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 /* eslint-disable no-unused-vars */
 const btnShowAnswer = document.querySelector('.btnShowAnswer');
 const answerText = document.querySelector('.answer');
-const inputs = document.querySelectorAll('[data-js="input-area"]');
+const inputs = document.querySelectorAll('[data-js="textarea"]');
 
 
 
-inputs?.forEach(item => {
+inputs.forEach(item => {
   const inputCounter = item.querySelector('[data-js="input-counter"]');
-  item.addEventListener('input', e => {
-    if (item.value.length <= 100) {
-      inputCounter.textContent = e.target.value.length + "/" + 100;
+  const inputArea = item.querySelector('[data-js="input-area"]');
+  inputArea.addEventListener('input', () => {
+    if (inputArea.value.length <= 100) {
+      inputCounter.textContent = inputArea.value.length + "/" + 100;
     // eslint-disable-next-line no-empty
     } else {
     }
